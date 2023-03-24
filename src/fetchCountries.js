bad3a9972cbddfcd3f4dca6225e55139b841c155
anastasia-demia <109302069+anastasia-demia@users.noreply.github.com>
2023-03-24 17:07:34 +0200

    minor changes

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,3 +1,11 @@
-function fetchCountries(name){
-  //body of a function
+export function fetchCountries(name){
+  const url = 'https://restcountries.com/v3.1/name/';
+    const filter = '?fields=name,capital,population,flags,languages';
+    return fetch(`${url}${name}${filter}`).then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+
+      return response.json();
+    });
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-
 const DEBOUNCE_DELAY = 300;
+
